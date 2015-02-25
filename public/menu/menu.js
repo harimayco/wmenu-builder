0ade8ec0f7b9739fe6eb1bf9130f41ee28325ef1
Crsitian Camilo Garcia <informacion@cristiangarcia.co>
2015-02-25 14:44:36 -0500

    Change menu algoritm

--- public/menu/menu.js
+++ public/menu/menu.js
@@ -1,37 +1,31 @@
-function getmenus() {     
-					$("#spinsavemenu").show()
-
-					var cont = 0;
-					$("#menu-to-edit li").each(function(index) {
-						var dept = 0;
-						for (var i = 0; i < $("#menu-to-edit li").length; i++) {
-
-							var n = $(this).attr("class").indexOf("menu-item-depth-" + i);
-							if (n != -1) {
-								dept = i;
-							}
-						};
-
-						var textoiner = $(this).find(".item-edit").context.outerText;
-
-						var textoexplotado = textoiner.split("|");
-						var padre = 0;
-						if (textoexplotado.length == 7) {
-							padre = textoexplotado[5]
-						}
-
-						var id = this.id.split("-");
-
-						arraydata.push({
-							depth : dept,
-							id : id[2],
-							parent : padre,
-							sort : cont
-						});
-						cont++;
-					});
-					actualizarmenu();
-				}
+function getmenus() {
+    $("#spinsavemenu").show()
+    var cont = 0;
+    $("#menu-to-edit li").each(function(index) {
+        var dept = 0;
+        for (var i = 0; i < $("#menu-to-edit li").length; i++) {
+            var n = $(this).attr("class").indexOf("menu-item-depth-" + i);
+            if (n != -1) {
+                dept = i;
+            }
+        };
+        var textoiner = $(this).find(".item-edit").context.outerText;
+        var textoexplotado = textoiner.split("|"); 
+        var padre = 0;  
+        if (!!textoexplotado[textoexplotado.length-2]) {  
+            padre = textoexplotado[textoexplotado.length-2]
+        }
+        var id = this.id.split("-");
+        arraydata.push({
+            depth : dept,
+            id : id[2],
+            parent : padre,
+            sort : cont
+        })
+        cont++;
+    });
+ 	actualizarmenu();
+}
 
 				function addcustommenu() {
 					$("#spincustomu").show();
