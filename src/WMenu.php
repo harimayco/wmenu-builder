e4bbc17411f9350bbc72ef033b699c3b1260a0c4
berni69 <berni.emerald@gmail.com>
2017-10-14 23:43:15 +0200

    - changed signature from list to get, listByName to getByName - Reformated file

--- src/WMenu.php
+++ src/WMenu.php
@@ -1,86 +1,89 @@
 <?php
+
 namespace Harimayco\Menu;
 
-use Illuminate\Http\Request;
 use App\Http\Requests;
-use App\Http\Controllers\Controller;
 use Harimayco\Menu\Models\Menus;
 use Harimayco\Menu\Models\MenuItems;
 
 class WMenu
 {
 
-	public function render(){
-		$menu = new Menus();
-		$menuitems = new MenuItems();
-		$menulist = $menu->select(['id', 'name'])->get();
-		$menulist = $menulist->pluck('name', 'id')->prepend('Select menu', 0)-> all();
+    public function render()
+    {
+        $menu = new Menus();
+        $menuitems = new MenuItems();
+        $menulist = $menu->select(['id', 'name'])->get();
+        $menulist = $menulist->pluck('name', 'id')->prepend('Select menu', 0)->all();
 
-		//$menulist[0] = "Select menu";
-		if ( (request()->has("action")  && empty(request()->input("menu"))) || request()->input("menu") == '0' ) {
-			return view('vendor.harimayco-menu.menu-html') -> with("menulist", $menulist);
-		} else {
+        if ((request()->has("action") && empty(request()->input("menu"))) || request()->input("menu") == '0') {
+            return view('vendor.harimayco-menu.menu-html')->with("menulist", $menulist);
+        } else {
 
-			$menu = Menus::find(request()->input("menu"));
-			$menus = $menuitems -> getall(request()->input("menu"));
+            $menu = Menus::find(request()->input("menu"));
+            $menus = $menuitems->getall(request()->input("menu"));
 
-			$data = [ 'menus' => $menus, 'indmenu' => $menu, 'menulist' => $menulist ];
-			//\Debugbar::info();
-			return view('vendor.harimayco-menu.menu-html', $data);
-		}
+            $data = ['menus' => $menus, 'indmenu' => $menu, 'menulist' => $menulist];
+            return view('vendor.harimayco-menu.menu-html', $data);
+        }
 
-	}
-
-	public function scripts(){
-		return view('vendor.harimayco-menu.scripts');
-	}
-
-	public function select($name="menu", $menulist = array()){
-		$html = '<select name="'.$name.'">';
-
-		foreach($menulist as $key => $val){
-			$active = '';
-			if(request()->input('menu') == $key){
-				$active = 'selected="selected"';
-			}
-			$html .= '<option '.$active.' value="'.$key.'">'.$val.'</option>';
-		}
-		$html .= '</select>';
-		return $html;
-	}
-
-    public static function listByName($name){
-        $menu_id = Menus::byName($name)->id;
-        return self::list($menu_id);
     }
 
-	public static function list($menu_id){
-		$menuItem = new MenuItems;
-		$menu_list = $menuItem->getall($menu_id);
+    public function scripts()
+    {
+        return view('vendor.harimayco-menu.scripts');
+    }
 
-		$roots = $menu_list->where('menu', $menu_id)->where('parent', '0');
-		
-		$items = self::tree($roots, $menu_list);
-		return $items;
-	}
+    public function select($name = "menu", $menulist = array())
+    {
+        $html = '<select name="' . $name . '">';
 
-	private static function tree($items, $all_items){
-		$data_arr = array();
-		$i = 0;
-		foreach($items as $item){
-			$data_arr[$i] = $item->toArray();
-			$find = $all_items->where('parent', $item->id);
+        foreach ($menulist as $key => $val) {
+            $active = '';
+            if (request()->input('menu') === $key) {
+                $active = 'selected="selected"';
+            }
+            $html .= '<option ' . $active . ' value="' . $key . '">' . $val . '</option>';
+        }
+        $html .= '</select>';
+        return $html;
+    }
 
-			$data_arr[$i]['child'] = array();
+    public static function getByName($name)
+    {
+        $menu_id = Menus::byName($name)->id;
+        return self::get($menu_id);
+    }
 
-			if($find->count()){
-				$data_arr[$i]['child'] = self::tree($find, $all_items);
-			}
+    public static function get($menu_id)
+    {
+        $menuItem = new MenuItems;
+        $menu_list = $menuItem->getall($menu_id);
 
-			$i++;
-		}
+        $roots = $menu_list->where('menu', $menu_id)->where('parent', '0');
 
-		return $data_arr;
-	}
+        $items = self::tree($roots, $menu_list);
+        return $items;
+    }
+
+    private static function tree($items, $all_items)
+    {
+        $data_arr = array();
+        $i = 0;
+        foreach ($items as $item) {
+            $data_arr[$i] = $item->toArray();
+            $find = $all_items->where('parent', $item->id);
+
+            $data_arr[$i]['child'] = array();
+
+            if ($find->count()) {
+                $data_arr[$i]['child'] = self::tree($find, $all_items);
+            }
+
+            $i++;
+        }
+
+        return $data_arr;
+    }
 
 }
