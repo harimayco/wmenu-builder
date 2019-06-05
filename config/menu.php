2c041e9b2f87d785a1233b17de82f797eb41ea38
Daniel S <admin@developeroncall.com>
2019-06-05 12:29:34 +0700

    Set use roles default
    Making the use of roles default to false for easier install

--- config/menu.php
+++ config/menu.php
@@ -1,25 +1,25 @@
 <?php
 
 return [
-	/* you can add your own middleware here */
+/* you can add your own middleware here */
 	
-	'middleware' => [],
+   'middleware' => [],
 
-	/* you can set your own table prefix here */
-	'table_prefix' => 'admin_',
+/* you can set your own table prefix here */
+    'table_prefix' => 'admin_',
 
-    /* you can set your own table names */
+/* you can set your own table names */
     'table_name_menus' => 'menus',
 
     'table_name_items' => 'menu_items',
 
-    /* you can set your route path*/
+/* you can set your route path*/
     'route_path' => '/harimayco/',
 
-    /* here you can make menu items visible to specific roles */
-    'use_roles' => true,
+/* here you can make menu items visible to specific roles */
+    'use_roles' => false,
 
-    /* If use_roles = true above then must set the table name, primary key and title field to get roles details */
+/* If use_roles = true above then must set the table name, primary key and title field to get roles details */
     'roles_table' => 'roles',
 
     'roles_pk' => 'id', // primary key of the roles table
