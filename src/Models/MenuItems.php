8b90c1f5adc932cc6f4fa434d88d7260ed77320b
Rendy Harimayco <harimayco@users.noreply.github.com>
2019-07-25 14:42:57 +0700

    fix sort new menu

--- src/Models/MenuItems.php
+++ src/Models/MenuItems.php
@@ -7,24 +7,27 @@ use Illuminate\Database\Eloquent\Model;
 class MenuItems extends Model
 {
 
-	protected $table = null;
+    protected $table = null;
 
     protected $fillable = ['label', 'link', 'parent', 'sort', 'class', 'menu', 'depth', 'role_id'];
 
-	public function __construct( array $attributes = [] ){
-    	//parent::construct( $attributes );
-    	$this->table = config('menu.table_prefix') . config('menu.table_name_items');
+    public function __construct(array $attributes = [])
+    {
+        //parent::construct( $attributes );
+        $this->table = config('menu.table_prefix') . config('menu.table_name_items');
     }
 
-    public function getsons($id) {
-		return $this -> where("parent", $id) -> get();
-	}
-	public function getall($id) {
-		return $this -> where("menu", $id) -> orderBy("sort", "asc")->get();
-	}
-
-	public static function getNextSortRoot($menu){
-        return self::where('menu',$menu)->where('depth',0)->max('sort') + 1;
+    public function getsons($id)
+    {
+        return $this->where("parent", $id)->get();
+    }
+    public function getall($id)
+    {
+        return $this->where("menu", $id)->orderBy("sort", "asc")->get();
     }
 
+    public static function getNextSortRoot($menu)
+    {
+        return self::where('menu', $menu)->max('sort') + 1;
+    }
 }
