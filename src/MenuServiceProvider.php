5762fffde410648afb6a2d76c3e16f2df24497fa
Rendy Harimayco <rendyharimayco@gmail.com>
2019-07-31 10:33:16 +0700

    fix wrong view vendor path #43

--- src/MenuServiceProvider.php
+++ src/MenuServiceProvider.php
@@ -14,28 +14,29 @@ class MenuServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (! $this->app->routesAreCached()) {
-                require  __DIR__.'/routes.php';
+        if (!$this->app->routesAreCached()) {
+            require  __DIR__ . '/routes.php';
         }
 
+        $this->loadViewsFrom(__DIR__ . '/Views', 'wmenu');
+
         $this->publishes([
-            __DIR__.'/../config/menu.php'  => config_path('menu.php'),
+            __DIR__ . '/../config/menu.php'  => config_path('menu.php'),
         ], 'config');
 
         $this->publishes([
-           __DIR__.'/Views'   => resource_path('views/vendor/harimayco-menu'),
+            __DIR__ . '/Views'   => resource_path('views/vendor/wmenu'),
         ], 'view');
 
-         $this->publishes([
-            __DIR__.'/../assets' => public_path('vendor/harimayco-menu'),
+        $this->publishes([
+            __DIR__ . '/../assets' => public_path('vendor/harimayco-menu'),
         ], 'public');
 
         $this->publishes([
-            __DIR__.'/../migrations/2017_08_11_073824_create_menus_wp_table.php' => database_path('migrations/2017_08_11_073824_create_menus_wp_table.php'),
-            __DIR__.'/../migrations/2017_08_11_074006_create_menu_items_wp_table.php' => database_path('migrations/2017_08_11_074006_create_menu_items_wp_table.php'),
-            __DIR__.'/../migrations/2019_01_05_293551_add-role-id-to-menu-items-table.php' => database_path('2019_01_05_293551_add-role-id-to-menu-items-table.php'),
+            __DIR__ . '/../migrations/2017_08_11_073824_create_menus_wp_table.php' => database_path('migrations/2017_08_11_073824_create_menus_wp_table.php'),
+            __DIR__ . '/../migrations/2017_08_11_074006_create_menu_items_wp_table.php' => database_path('migrations/2017_08_11_074006_create_menu_items_wp_table.php'),
+            __DIR__ . '/../migrations/2019_01_05_293551_add-role-id-to-menu-items-table.php' => database_path('2019_01_05_293551_add-role-id-to-menu-items-table.php'),
         ], 'migrations');
-        
     }
 
     /**
@@ -45,14 +46,14 @@ class MenuServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind('harimayco-menu', function() {
+        $this->app->bind('harimayco-menu', function () {
             return new WMenu();
         });
 
         $this->app->make('Harimayco\Menu\Controllers\MenuController');
-        $this->loadViewsFrom(__DIR__.'/views', 'wmenu');
         $this->mergeConfigFrom(
-            __DIR__ . '/../config/menu.php', 'menu'
+            __DIR__ . '/../config/menu.php',
+            'menu'
         );
     }
 }
